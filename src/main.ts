a1d85aa23f05fbdf38c43486faa7c5981417bdd1
Dave Mcsavvy <davemcsavvii@gmail.com>
2025-07-15 08:14:55 +0100

    Add swagger-ui-express for API documentation and configure Swagger setup in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './config/exception';
 import { NestExpressApplication } from "@nestjs/platform-express";
 import helmet from "helmet";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -19,6 +20,16 @@ async function bootstrap() {
   app.setGlobalPrefix('api'); // Set a global prefix for all routes app.useGlobalPipes(new ValidationPipe({ transform: true }));
   const httpAdapter = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  // Swagger/OpenAPI setup
+  const config = new DocumentBuilder()
+    .setTitle('Reatent API')
+    .setDescription('API documentation for Reatent')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
